efb55ebc46377ff05a92cf9a26b0ef60bf07c64f
Shain Dholakiya <shainshain99@gmail.com>
2018-12-06 11:08:48 -0500

    initial site files

---
+++ assets/javascript/game.js
@@ -0,0 +1,63 @@
+var alphabetArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
+var wins = 0;
+
+var losses = 0;
+
+var guessesLeft = 10;
+
+var guessesSoFar = 0;
+
+var guessedLetters = [];
+
+alert("Click any letter to guess which letter the computer is thinking of");
+
+document.onkeyup = function (event) {
+    
+    var userGuess = event.key;
+    
+    var computerGuess = alphabetArray[Math.floor(Math.random() * alphabetArray.length)];
+    
+    if (userGuess === computerGuess) {
+        
+        alert("You have guessed the correct letter! " + computerGuess);
+        
+        wins++
+        
+        guessesLeft = 10;
+        
+        guessedLetters = [];
+    
+    }
+    
+    if (userGuess != computerGuess) {
+        
+        guessedLetters.push(userGuess);
+        
+        guessesLeft--;
+        
+        guessesSoFar++;
+    
+    }
+    
+    if (guessesLeft === 0) {
+        
+        alert("You lost. Try again.");
+        
+        guessesLeft = 10;
+        
+        losses++;
+        
+        guessedLetters = [];
+    
+    }
+    
+    document.getElementById("wins").innerHTML = ("Wins: " + wins);
+    
+    document.getElementById("losses").innerHTML = ("Losses: " + losses);
+    
+    document.getElementById("guessesLeft").innerHTML = ("Guesses Left: " + guessesLeft);
+    
+    document.getElementById("guessesSoFar").innerHTML = ("Your guesses so far: " + guessedLetters);
+
+}
